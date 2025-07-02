6109f13f5c5110c9902172154e3e983a27700adc
Dikeydevil <purginkirill17@gmail.com>
2025-07-02 22:07:45 +0300

    add pattern repository

--- src/api/dependecies.py
+++ src/api/dependecies.py
@@ -3,11 +3,10 @@ from typing import Annotated
 from fastapi import Depends, Query
 from pydantic import BaseModel
 
-
 class PaginationParams(BaseModel):
-    page: Annotated [int | None, Query( None, ge=1, description="Номер страницы")]
-    per_page: Annotated [int | None, Query( None, ge=1, le=100, lt=300, description="Количество элементов на страницу")]
-
-
+    # по умолчанию page=1, обязательно int, минимум 1
+    page: Annotated[int, Query(1, ge=1, description="Номер страницы")]
+    # по умолчанию per_page=5, обязательно int, от 1 до 100
+    per_page: Annotated[int, Query(5, ge=1, le=100, description="Элементов на страницу")]
 
 PaginationDep = Annotated[PaginationParams, Depends()]
--- src/api/hotels.py
+++ src/api/hotels.py
@@ -21,31 +21,14 @@ async def get_hotels(
     location: str | None = Query(default=None, description="Локация отеля"),
     title: str | None = Query(default=None, description="Название отеля"),
 ):
+    per_page=pagination.per_page or 5
     async with async_session_maker() as session:
-        return await HotelsRepository(session).get_all()
-    # per_page = pagination.per_page or 5
-    # async with async_session_maker() as session:
-    #     query = select(HotelsOrm)
-    #
-    #     filters = []
-    #     if location:
-    #         filters.append(HotelsOrm.location.ilike(f"%{location}%"))
-    #     if title:
-    #         filters.append(HotelsOrm.title.ilike(f"%{title}%"))
-    #     if filters:
-    #         query = query.filter(*filters)
-    #
-    #     query = (
-    #         query
-    #         .limit(per_page)
-    #         .offset(per_page * (pagination.page - 1))
-    #     )
-    #     print(query.compile(compile_kwargs={"literal_binds": True}))
-    #     result = await session.execute(query)
-    #     hotels = result.scalars().all()
-    #
-    # return hotels
-
+        return await HotelsRepository(session).get_all(
+            location=location,
+            title=title,
+            limit=per_page,
+            offset=per_page * (pagination.page - 1)
+        )
 
 
 @router.post("", summary="Создание нового отеля",
