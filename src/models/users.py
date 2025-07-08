59b2ce85978aaf95649ee6487f736184712c28aa
Dikeydevil <purginkirill17@gmail.com>
2025-07-08 23:32:28 +0300

    unique email & username

--- src/models/users.py
+++ src/models/users.py
@@ -7,8 +7,8 @@ class UsersOrm(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(100))
-    username: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(100),unique=True)
+    username: Mapped[str] = mapped_column(String(100),unique=True)
     firstname: Mapped[str] = mapped_column(String(100))
     lastname: Mapped[str] = mapped_column(String(100))
     hashed_password: Mapped[str] = mapped_column(String(256))
